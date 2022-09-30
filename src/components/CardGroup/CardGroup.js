915f12884f8bc9402d59343316c2ef32057c934a
opuzayed <zayedopu4933@gmail.com>
2022-09-30 22:37:23 +0600

    added custom data in card

--- src/components/CardGroup/CardGroup.js
+++ src/components/CardGroup/CardGroup.js
@@ -1,33 +1,18 @@
 import React from 'react';
+import Card from '../Card/Card';
 
 const CardGroup = () => {
+    const products = [
+        {id:1, name:'macbook', price : 287845},
+        {id:2, name:'probook', price : 287845},
+        {id:3, name:'surfacebook', price : 287845}
+    ];
     return (
         <div className="card-group">
-  <div className="card">
-    <img src="..." className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div className="card">
-    <img src="..." className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div className="card">
-    <img src="..." className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-</div>
+            {
+                products.map(product =><Card key={product.id} product={product}></Card>)
+            }
+        </div>
     );
 };
 
