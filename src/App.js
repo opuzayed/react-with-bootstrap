33c0c3326779b034341627dac8c291bac41a74aa
opuzayed <zayedopu4933@gmail.com>
2022-09-30 22:05:05 +0600

    card added from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import CardGroup from "./components/CardGroup/CardGroup";
 
 function App() {
-  return (
-    <div className="App">
-       <button type="button" className="btn btn-primary">Primary</button>
-      <button type="button" className="btn btn-secondary">Secondary</button>
-      <button type="button" className="btn btn-success">Success</button>
-      <button type="button" className="btn btn-danger">Danger</button>
-      <button type="button" className="btn btn-warning">Warning</button>
-      <button type="button" className="btn btn-info">Info</button>
-      <button type="button" className="btn btn-light">Light</button>
-      <button type="button" className="btn btn-dark">Dark</button>
-
-      <button type="button" className="btn btn-link">Link</button>   
-    </div>
-  );
+  return <div className="App">
+    <CardGroup></CardGroup>
+  </div>;
 }
 
 export default App;
